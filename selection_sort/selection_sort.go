d0cd0b0c8f205b28f9fd14d4d7061bf2b7d8fe65
francis <a0975568423@gmail.com>
2026-03-16 18:12:13 +0800

    refactor: implement selec tion sort with in-place array swapping
    Use element swapping instead of creating new array.
    First approach kept as comments for syntax research.

--- selection_sort/selection_sort.go
+++ selection_sort/selection_sort.go
@@ -4,32 +4,48 @@ import (
 	"fmt"
 )
 
-func FindSmallest(arr []int) int {
+// // 1. create empty array and put smallest number into it
+// func FindSmallest(arr []int) int {
 
-	number := arr[0]
+// 	number := arr[0]
 
-	for index, _ := range arr {
-		if arr[index] < number {
-			number = arr[index]
+// 	for index, _ := range arr {
+// 		if arr[index] < number {
+// 			number = arr[index]
+// 		}
+// 	}
+// 	// index := slices.Index(arr, number)
+// 	// slices.Delete(arr, index, index+1)
+// 	// fmt.Println("array: ", arr)
+// 	return number
+// }
+
+// func SelectSort(arr []int) []int {
+// 	result := make([]int, len(arr))
+
+// 	for index, _ := range arr {
+// 		result[index] = FindSmallest(arr)
+// 	}
+// 	return result
+// }
+
+// 2. exchange smallest number in current array
+func SelectSort_2(arr []int) []int {
+	length := len(arr)
+	for i := 0; i < length-1; i++ {
+		minIdx := i
+		for j := i + 1; j < length; j++ {
+			if arr[j] < arr[minIdx] {
+				minIdx = j
+			}
 		}
+		arr[i], arr[minIdx] = arr[minIdx], arr[i]
 	}
-	// index := slices.Index(arr, number)
-	// slices.Delete(arr, index, index+1)
-	// fmt.Println("array: ", arr)
-	return number
-}
-
-func SelectSort(arr []int) []int {
-	result := make([]int, len(arr))
-
-	for index, _ := range arr {
-		result[index] = FindSmallest(arr)
-	}
-	return result
+	return arr
 }
 
 func main() {
 	arr := []int{5, 3, 6, 2, 10}
-	result := SelectSort(arr)
+	result := SelectSort_2(arr)
 	fmt.Println(result)
 }
--- selection_sort/selection_sort_test.go
+++ selection_sort/selection_sort_test.go
@@ -1,23 +1,26 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSelectSort(t *testing.T) {
 	arr := []int{100, 57, 61, 4, 7}
-	got := SelectSort(arr)
+	got := SelectSort_2(arr)
 	want := []int{4, 7, 57, 61, 100}
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("wanted %v got %v", want, got)
 	}
 }
 
-func TestFindSmallest(t *testing.T) {
-	arr := []int{3, 4, 5, 1, 2}
-	got := FindSmallest(arr)
-	want := 1
+// func TestFindSmallest(t *testing.T) {
+// 	arr := []int{3, 4, 5, 1, 2}
+// 	got := FindSmallest(arr)
+// 	want := 1
 
-	if got != want {
-		t.Errorf("wanted %d got %d", want, got)
-	}
-}
+// 	if got != want {
+// 		t.Errorf("wanted %d got %d", want, got)
+// 	}
+// }
