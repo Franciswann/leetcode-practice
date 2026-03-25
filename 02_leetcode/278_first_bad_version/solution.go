bdea5f8ebb7bede1916fdd23ea114b23fee860db
francis <a0975568423@gmail.com>
2026-03-25 16:24:49 +0800

    refactor: simplify binary search implementation for first bad version

--- 02_leetcode/278_first_bad_version/solution.go
+++ 02_leetcode/278_first_bad_version/solution.go
@@ -15,23 +15,29 @@ package first_bad_version_278
  * func isBadVersion(version int) bool;
  */
 
-func FirstBadVersion(n int) int {
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
 	// if n = 5 ->
 	// [1, 2, 3, 4, 5]
 
 	right := n
 	left := 1
 
-	for left <= right {
+	for left < right {
 		mid := left + (right-left)/2
-		if isBadVersion(mid) == true {
-			if left == mid {
-				return mid
-			}
-			right = mid - 1
+		if isBadVersion(mid) {
+			// first bad version might be mid or on the left side
+			right = mid
 		} else {
 			left = mid + 1
 		}
 	}
-	return 0
+	return left
 }
