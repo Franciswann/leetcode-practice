a3200ce78f3925306a6c91a0e80aed373e8bc466
francis <a0975568423@gmail.com>
2026-04-08 17:33:59 +0800

    feat: setup Dijkstra algorithm data structure and graph definition

---
+++ 01_basics/dijkstra/dijkstra.go
@@ -0,0 +1,33 @@
+package main
+
+// Elements: START, A, B, FINAL
+
+// START -> A : 6
+// START -> B : 2
+
+// A -> FIN : 1
+
+// B -> A : 3
+// B -> FIN : 5
+
+// FIN : infinity
+// Infinity represents an unreachable distance
+// const Infinity = math.MaxInt32
+
+// Graph
+// key : string (node)
+// value : map[string]int (neighbor: weight)
+
+// Graph := make(map[string]map[string]int)
+
+// Costs := make(map[string]int)
+
+// Parent := make(map[string]string)
+
+// Processed := []string{}
+
+// func find_lowest_cost_node(costs Costs, processed []string) (string, int)
+
+func main() {
+
+}
